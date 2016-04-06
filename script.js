37ce0674059cead259a83ad172b4658f7739ce0e
Martin Jensen <martin2610@gmail.com>
2016-04-06 20:37:35 +0200

    ReplicationControllers changed to Deployments
    Quick fix to swap ReplicationController with Deployments

--- script.js
+++ script.js
@@ -316,11 +316,10 @@ var loadData = function() {
 			}
 		});
 	});
-
-	var req2 = $.getJSON("/api/v1/replicationcontrollers?labelSelector=visualize%3Dtrue", function( data ) {
+	var req2 = $.getJSON("/apis/extensions/v1beta1/namespaces/default/deployments/?labelSelector=visualize%3Dtrue", function( data ) {
 		controllers = data;
 		$.each(data.items, function(key, val) {
-			val.type = 'replicationController';
+			val.type = 'deployment';
       //console.log("Controller ID = " + val.metadata.name)
   });
 	});
